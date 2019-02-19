3ce02317e9608568ed7d61722c7a559f6fecdb40
James Jarrett <james.jarrett@okstate.edu>
2019-02-18 22:20:11 -0600

    Basic form setup//render issues for some reason

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './components/form'
+import Main from './components/form';
 
 class App extends Component {
   render() {
     return (
-      <Form/>
+      <Main />
     );
   }
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,115 +1,166 @@
-// // import React from 'react'
-// // import { Formik, Form, Field, ErrorMessage } from 'formik'
 
-// // const Basic = () => (
-// //     <div>
-// //         <h1>AQR Form Demo</h1>
-// //         <Formik
-// //         initialValues={{ email: 'email', password: 'password'}}
-// //         validate={values => {
-// //             let errors = {};
-// //             if (!values.email) {
-// //                 errors.email = 'Required';
-// //             } else if ( 
-// //                 !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-// //             ) {
-// //                 errors.email = 'Invalid email address';
-// //             } 
-// //             console.log(errors)
-// //             return errors;
-// //         }}
-// //         onSubmit={(values, { setSubmitting }) => {
-// //             setTimeout(() => {
-// //                 alert(JSON.stringify(values, null, 2));
-// //                 setSubmitting(false);
-// //             }, 400);
-
-// //         }}
-// //         >
-// //         {({ isSubmitting }) => (
-// //             <Form>
-// //                 <Field type="email" name="email" />
-// //                 <ErrorMessage name="email" component="div" />
-// //                 <Field type="password" name="password" />
-// //                 <ErrorMessage name="password" component="div" />
-// //                 <button type="submit" disabled={isSubmitting}>
-// //                     Submit
-// //                 </button>
-// //             </Form>
-// //         )}
-// //         </Formik>
-// //     </div>
-// // );
-
-// // export default Basic
-
-
-// import React from 'react'
-// import { Formik, FormikProps, Form, Field } from 'formik'
-
-// export class AQRForm extends React.Component {
-//     handleSubmit = (values, {
-//         props = this.props,
-//         setSubmitting
-//     }) => {
-
-//         //Process form submission here
-
-//         //on done submit set submitting to flase
-//         setSubmitting(false)
-
-//         return;
-//     }
-
-//     render() {
-
-//         return (
-//             <Formik
-//                 initialValues={{
-//                     first_name: '',
-//                     email_address: '',
-//                     gender: ''
-//                 }}
-//                 validate={(values) => {
-//                     let errors = [];
-
-//                     // check if my values have any errors 
-//                     return errors;
-//                 }}
-//                 onSubmit={handleSubmit}
-//                 render={formProps: FormikProps => {
-
-//             return(
-//                 <Form>
-//                     <Field />
-//                     <Field />
-//                     <Field />
-
-//                     <button
-//                         type="submit"
-//                         disabled={formProps.isSubmitting}>
-//                         Submit Form
-//                     </button>
-//                 </Form>
-//             );
-//     }}
-import React from 'react';
-import { render } from 'react-dom';
-import Styles from './styles';
+import React, { Component } from 'react';
 import { Form, Field } from 'react';
-import AutoSave from './AutoSave';
+import  Styles  from './styles';
+import  AutoSave  from './AutoSave';
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const save = async values => {
-    console.log('Saving', values)
-    await sleep(2000)
+    console.log('Saving', values);
+    await sleep(2000);
 }
-
-const App = () => (
-    <h1>AQR Final Form </h1>
-
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {save, sleep};
+    }
+    render() {
+        const {sleep, save, } = this.state;
+        return (
+    <Styles>
+        <h1>AQR Final Form </h1>
     <Form 
         onSubmit={save}
-        initialValies={{}}
-)
+        initialValies={{state: 'TX'}}
+        subscription={{}/* Email subscriber will be added here*/}
+    >
+        {() => (
+            <div className="form">
+                <AutoSave debounce={1000} save={save} />>        
+            <div>
+                <label> First Name</label>
+                <Field 
+                    name="firstName"
+                    component="input"
+                    type="text"
+                    placeholder="First Name"
+                />
+            </div>
+            <div>
+            <label> Last Name</label>
+            <Field 
+                name="lastName"
+                component="input"
+                type="text"
+                placeholder="Last Name"
+            />
+        </div>
+        <div>
+            <label>Phone Number</label>
+            <Field
+                name="phoneNumber"
+                component="input"
+                type="text"
+                placeholder="#"  
+            />
+        </div>
+        <div>
+            <label>Email</label>
+            <Field
+                name="email"
+                componet="input"
+                type="email"
+                placeholder="Email"
+            />
+        </div>
+        <h2>Contact Info</h2>
+        <div>
+            <label> First Name</label>
+            <Field 
+                name="firstName"
+                component="input"
+                type="text"
+                placeholder="First Name"
+            />
+        </div>
+        <div>
+        <label> Last Name</label>
+        <Field 
+            name="lastName"
+            component="input"
+            type="text"
+            placeholder="Last Name"
+        />
+    </div>
+    <div>
+        <label>Phone Number</label>
+        <Field
+            name="phoneNumber"
+            component="input"
+            type="text"
+            placeholder="#"  
+        />
+    </div>
+    <div>
+        <label>Email</label>
+        <Field
+            name="email"
+            componet="input"
+            type="email"
+            placeholder="Email"
+        />
+    </div>
+        <h2>Property Address</h2>
+    <div>
+        <label>Address Line:</label>
+        <Field
+            name="addressLine"
+            component="input"
+            type="address"
+            placeholder="Address"
+        />
+    </div>
+    <div>
+        <label>City:</label>
+        <Field
+            name="city"
+            component="address"
+            type="city"
+            placeholder="Texas"
+        />
+    </div>
+    <div>
+        <label>State</label>
+        <Field
+            name="state"
+            component="address"
+            type="state"
+            placeholder="TX"
+        />
+    </div>
+    <div>
+        <label>Zip Code</label>
+        <Field
+            name="zipCode"
+            component="address"
+            type="zip"
+            placeholder="11111"
+        />
+    </div>
+        <h2>Type of Problem</h2>
+    <div>
+        <label>Choose an item</label>
+        <Field
+        name="problemType"
+        component="select"
+        >
+        <option />
+        <option value ="Option 1"></option>
+        <option value ="Option 2"></option>
+        <option value ="Option 3"></option>
+        </Field>
+    </div>
+    <div>
+        <label>Description of Problem/Work</label>
+        <Field name="description" component="textarea" placeholder="Description of Problem" />>
+    </div>
+    </div>
+    )}
+    </Form>
+    </Styles>
+    );
+}
+}
+
+export default App
